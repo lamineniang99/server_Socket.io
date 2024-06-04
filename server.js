1618cbd62f3b71c6dd38e2fe4ac3cb3b1cd07089
lamineniang99 <pisupniang@gmail.com>
2024-06-04 13:31:01 +0000

    update server loc

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const { Server } = require('socket.io');
 const net = require('net');
 
+const fs = require('fs');
+const https = require('https');
+
+
 const io = new Server({
     cors: {
-        origin: 'http://localhost:4200',
+        origin: 'https://sukali.nautx.sn',
         methods: ["GET", "POST"]
     }
 });
@@ -70,7 +74,10 @@ tcpClient.on('data', (data) => {
         message = JSON.parse(message);
         console.log(message.imei);
         if (message && message.imei) {
-            let roomName = `imei_${message.imei}`;
+            let roomName = `${message.imei}`;
+	    let canal = roomName +  "update";
+	    console.log("Canal name" , roomName);
+	    io.emit( canal,  message);
             io.to(roomName).emit('update', message); // Envoyer les données à tous les clients dans cette room
         }
     } catch (error) {
@@ -88,113 +95,3 @@ const PORT = process.env.PORT || 3300;
 io.listen(PORT);
 console.log(`Serveur Socket.IO démarré sur le port ${PORT}`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const { Server } = require('socket.io');
-// const net = require('net');
-
-
-// const server = new Server({ cors: { 'origin': 'http://localhost:4200' } });
-
-// // Créer une connexion TCP vers le serveur cible
-// const tcpClient = new net.Socket();
-// const TCP_SERVER_HOST = "44.204.65.69";  // Modifier l'hôte du serveur TCP cible
-// const TCP_SERVER_PORT = 3000; // Modifier le port du serveur TCP cible
-
-// // tcpClient.connect(TCP_SERVER_PORT, TCP_SERVER_HOST, () => {
-// //     console.log('Connecté au serveur TCP');
-// // });
-
-
-// let tcpReconnectionTimer;
-
-// function connectTCP() {
-//     tcpClient.connect(TCP_SERVER_PORT, TCP_SERVER_HOST, () => {
-//       console.log('Connecté au serveur TCP');
-//       clearTimeout(tcpReconnectionTimer);
-//     });
-    
-//     tcpClient.on('error', (error) => {
-//       console.log('Erreur de connexion TCP:', error.message);
-//       tcpClient.end();
-//       tcpReconnectionTimer = setTimeout(connectTCP, 5000); // Tenter une reconnexion après 5 secondes
-//     });
-//   }
-  
-//   connectTCP();
-  
-
-     
-// // Gestion des connexions Socket.IO
-// server.on('connection', (socket) => {
-
-//     console.log('Un client s\'est connecté');
-
-//     // Gestion des messages Socket.IO
-//     socket.on('data', (data) => {
-//         //console.log('Message reçu:', data);
-//         const perimetre = (data.perimetre.position == 'left')? '00':'01'
-//         const status1 = data.perimetre.status? '01':'00'
-//         let dataToSend = data.type+" - "+data.imei+" - 0"+data.position+" - "+perimetre+" - "+status1
-//         console.log("les données a envoyer cote server",dataToSend);
-//         // let message = JSON.parse(data) ;
-//         // console.log("data after filtrage", message);
-//         // Transférer les données au serveur TCP
-//         tcpClient.write(dataToSend);
-//     });
- 
-       
-//     // Gestion de la déconnexion Socket.IO
-//     socket.on('disconnect', () => {
-//         console.log('Un client s\'est déconnecté');
-//     });
-// });
-
-// // Gestion des événements du serveur TCP
-// tcpClient.on('data', (data) => {
-//     console.log('Données reçues du serveur TCP:', data.toString());
-// });
-
-// tcpClient.on('close', () => {
-//     console.log('Connexion TCP fermée')
-//     connectTCP();
-// });
-
-// // Démarrer le serveur Socket.IO
-// const PORT = process.env.PORT || 3300;
-// server.listen(PORT);
-// console.log(`Serveur Socket.IO démarré sur le port ${PORT}`);
